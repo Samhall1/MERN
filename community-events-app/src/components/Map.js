8dcaadb7a41183aac917b5baf83336cda62a6cef
SamH <Sam.Hall@ao.com>
2022-03-11 21:18:08 +0000

    chore: adds new marker and info window to map

--- community-events-app/src/components/Map.js
+++ community-events-app/src/components/Map.js
@@ -1,5 +1,5 @@
 import { Loader } from "@googlemaps/js-api-loader";
-import React from "react";
+import React, { useEffect } from "react";
 import "./Map.css";
 
 const loader = new Loader({
@@ -7,25 +7,64 @@ const loader = new Loader({
   version: "weekly",
 });
 
-let map;
+const testWrapper = (events) => {
+  let map;
 
-loader.load().then(() => {
-  const google = window.google;
   const myLoc = { lat: 53.480759, lng: -2.242631 };
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: myLoc,
-    zoom: 8,
-  });
-  new google.maps.Marker({
-    position: myLoc,
-    map,
-  });
-});
 
-const Map = () => {
+  loader.load().then(() => {
+    const google = window.google;
+    map = new google.maps.Map(document.getElementById("map"), {
+      center: myLoc,
+      zoom: 10,
+    });
+
+    events.forEach((event) => {
+      const marker = new google.maps.Marker({
+        position: { lat: event.eventLat, lng: event.eventLong },
+        map,
+      });
+
+      const contentString =
+        '<div id="content">' +
+        "<li>" +
+        `${event.eventTitle}` +
+        "<li>" +
+        `${event.description}` +
+        "<li>" +
+        `${event.startDate}` +
+        "<li>" +
+        `${event.startTime}` +
+        "<li>" +
+        `${event.city}` +
+        "</div>";
+
+      const infowindow = new google.maps.InfoWindow({
+        content: contentString,
+      });
+
+      marker.addListener("click", () => {
+        infowindow.open({
+          anchor: marker,
+          map,
+          shouldFocus: false,
+        });
+      });
+    });
+  });
+};
+
+const Map = ({ events }) => {
+  useEffect(() => {
+    testWrapper(events);
+  }, [events]);
+
   return (
     <div className="map-container">
       <div id="map"></div>
+      {events.map((event) => {
+        return <div>{event.city}</div>;
+      })}
     </div>
   );
 };
